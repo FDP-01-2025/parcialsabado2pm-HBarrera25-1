7b39017a641146f492e8f1757ecb35f7cf7df274
HBarrera25 <00226325@uca.edu.sv>
2025-06-07 16:01:31 -0600

    parcial00226325.cpp

--- parcial00226325.cpp
+++ parcial00226325.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-    int n, i;
-    cout << "--PROGRAMA SERIE DE NUMERO N--" <<endl <<endl;
-    cout << "Ingresa tu numero: " <<endl;
-    cin >> n;
+int main() {
+    int N;
+    cout << "Ingresa un numero N: ";
+    cin >> N;
 
-    for(int i = 1; i <= n; i++){
-       cout << i;
-    if ( i < n){
-        cout << " / " <<n <<endl;
-        cout << endl;
-    } 
-   
-
-        return 0;
-    
+    for (int i = 1; i <= N; i++) {
+        cout << "1/" << i;
+        if (i < N) cout << ", ";
     }
+    cout << endl;
+
+    return 0;
 }
