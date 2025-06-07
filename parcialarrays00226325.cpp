8d0a7183de5f89bf8da43a6bc7fa005a7fc1aada
HBarrera25 <00226325@uca.edu.sv>
2025-06-07 15:52:53 -0600

    parcialarrays00226325.cpp

--- parcialarrays00226325.cpp
+++ parcialarrays00226325.cpp
@@ -1,14 +1,25 @@
-#include <iostream>
+#include using namespace std;
 
-using namespace std;
+int main() {
+int arr[15];
+int mayor, menor;
 
-int main(){
-    int num;
-    cout << "Ingresa tu numero: " <<endl;
-    cin  >> num;
-    num [15];
-    if (num[15] > num [15]){
-        cout << ""
-    }
+cout << "Ingresa 15 numeros:" << endl;
+for (int i = 0; i < 15; i++) {
+cout << "Numero " << i + 1 << ": ";
+cin >> arr[i];
+}
 
+mayor = menor = arr[0];
+for (int i = 1; i < 15; i++) {
+if (arr[i] > mayor)
+mayor = arr[i];
+if (arr[i] < menor)
+menor = arr[i];
+}
+
+cout << "Mayor: " << mayor << endl;
+cout << "Menor: " << menor << endl;
+
+return 0;
 }
